a28f10893282b9d7668bfbfab4dc501f635423a0
gim-ujae <kwj1270@naver.com>
2020-11-28 19:13:50 +0900

    feat: Create BaseBallGame.java

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import utils.RandomUtils;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BaseBallGame {
+    // 컴퓨터 클래스
+    // 사용자 클래스
+
+    // 기타 상수들 - 클래스로 분리해도 될 것 같은데
+    private final int RE_GAME = 1;
+    private final int END_GAME = 2;
+
+    public BaseBallGame() {
+        // 컴퓨터 클래스 초기화
+        // 사용자 클래스 초기화 메서드 실행
+    }
+
+    // 베이스볼을 시작하는 run 메서드
+    public void run(Scanner scanner) {
+
+    }
+
+}
+
+
