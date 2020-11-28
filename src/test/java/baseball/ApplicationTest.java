e1bc732e8d63679314f58f0e0e646b9b781d0621
gim-ujae <kwj1270@naver.com>
2020-11-28 18:59:14 +0900

    test: Create ApplicationTest.java

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,14 @@
 package baseball;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class ApplicationTest {
 
+    @Test
+    public void helloWorld() {
+        String hello = "Hello";
+        assertEquals(hello, "Hello");
+    }
 }
